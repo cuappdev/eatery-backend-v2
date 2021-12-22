5c5cd19232f66257558cf88a596fae581f045a7d
William Ma <wm274@cornell.edu>
2021-12-22 14:48:05 -0500

    Add Google Sheets for external eatery events

--- src/urls.py
+++ src/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("gs", views.google_sheets_eateries, name="gs")
+]
