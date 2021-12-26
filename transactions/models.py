b0136188831c4b1df9dc13cd9765db5df7dcc7ff
connorreinhold <cyreinhold@gmail.com>
2021-12-26 14:21:02 -0500

    Finish wait time data ingestion

--- transactions/models.py
+++ transactions/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # [transaction_count] transactions at [name] in time range [timestamp - 5 minutes, timestamp] on [canonical_date]
 class TransactionHistory(models.Model):
     class Meta:
-        unique_together = ('name', 'canonical_date', 'timestamp')
+        unique_together = ('name', 'timestamp', 'canonical_date')
         # idea is when fetching wait time for [name] in between [timestamp - 5, timestamp], we can look at the last 2 weeks of data for that [name, timestamp] pair
         indexes = [models.Index(fields = ['name', 'timestamp', 'canonical_date'])]  
     name = models.CharField(max_length=100)
