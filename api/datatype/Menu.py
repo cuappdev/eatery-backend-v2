f8ee7b45ff33d4e57e7ed1810d5f558c29576dfb
connorreinhold <cyreinhold@gmail.com>
2021-12-28 11:58:35 -0500

    Combine cafes and dining halls

---
+++ api/datatype/Menu.py
@@ -0,0 +1,9 @@
+from api.datatype.MenuCategory import MenuCategory
+
+class Menu:
+
+    def __init__(self, categories: list[MenuCategory]):
+        self.categories = categories
+
+    def to_json(self):
+        return [category.to_json() for category in self.categories]
