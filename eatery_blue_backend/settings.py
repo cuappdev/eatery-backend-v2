ca4bd4d5885914e322d5d4e01bf51cada47ab24f
connorreinhold <cyreinhold@gmail.com>
2022-01-08 09:54:15 -0500

    Move allowed hosts to env vars

--- eatery_blue_backend/settings.py
+++ eatery_blue_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-tup==8h@6!ewid!sfi*)jomsejj4j@=w=u*2ri9g0*0$3)1dkq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["d706-2601-187-8400-2076-1158-3d93-7b45-1456.ngrok.io", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
