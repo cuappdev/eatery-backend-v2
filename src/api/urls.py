64fd75e64239174a4e9b011f82cac2ee9ff7d778
Archit404Error <4architmehta@gmail.com>
2022-03-04 01:30:28 -0500

    Implement update endpoint for text params, define allowed fields, and add image encoding

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views import MainDfgView, ReportView
+from api.views import MainDfgView, UpdateView, ReportView
 
 urlpatterns = [
     path("", MainDfgView.as_view(), name="main"),
+    path("update", UpdateView.as_view(), name="update"),
     path("report", ReportView.as_view(), name="report")
 ]
