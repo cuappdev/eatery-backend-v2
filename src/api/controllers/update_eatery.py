2edc3d389dc8a3de4ba32c6bf69ff0bc3f21e24d
Archit404Error <4architmehta@gmail.com>
2022-03-05 23:03:23 -0500

    Added image uploading and exception handling

--- src/api/controllers/update_eatery.py
+++ src/api/controllers/update_eatery.py
@@ -1,46 +1,68 @@
 from django.http import QueryDict
 from api.models import EateryStore
 import base64
-import asyncio
+import requests
+import os
 
 
 class UpdateEateryController:
     def __init__(self, id: int, update_map: QueryDict, image):
-        '''
-        Update_map is a dictionary that maps the fields we want to update to 
+        """
+        Update_map is a dictionary that maps the fields we want to update to
         the values we want to map them to
 
-        Requires: id is a valid id and all keys in update_map are valid columns
-        '''
+        Requires: id is a valid id and all keys in update_map are valid fields
+            in the EateryStore class
+        Raises: Exception when invalid keys are provided
+        """
         self.id = id
         self.update_data = {}
-        allowed_fields = ["name", "menu_summary", "location", "campus_area", "online_order_url", "latitude",
-                          "longitude", "payment_accepts_meal_swipes", "payment_accepts_brbs", "payment_accepts_cash"]
 
         if image is not None:
-            asyncio.run(self.upload_image(image))
+            img_url = self.upload_image(image)
+            self.update_data["image_url"] = img_url
 
         for key, val in update_map.items():
-            if key in allowed_fields:
+            try:
                 self.update_data[key] = val
+            except:
+                raise Exception("Invalid update field(s) provided")
 
-    async def upload_image(self, image):
-        '''
+    def upload_image(self, image):
+        """
         Helper method that asynchronously uploads image bytes to assets repo
         Returns: stored image URL
-        '''
+        Raises: Exception invalid file extension when a non jpg, jpeg, gif, or png is provided
+        """
+
+        valid_extensions = ["jpg", "jpeg", "gif", "png"]
+        extension = (str(image)).split(".")
+        extension = extension[len(extension) - 1]
+
+        if extension not in valid_extensions:
+            raise Exception("Invalid File Extension")
 
         b64_encoded_image = b""
         # Encodes bytes in chunks to handle large image files efficiently
         for chunk in image.chunks():
             b64_encoded_image += base64.b64encode(chunk)
 
-        print(b64_encoded_image)
+        b64_encoded_image = b64_encoded_image.decode("utf-8")
+
+        response = requests.post(
+            "https://upload.cornellappdev.com/upload/",
+            json={
+                "bucket": os.environ["IMAGE_BUCKET"],
+                "image": f"data:image/{extension};base64,{b64_encoded_image}",
+            },
+        )
+
+        return response.json()["data"]
 
     def process(self):
-        '''
-        Selects the DB entry we want to update and updates it using provided data
-        '''
+        """
+        Selects DB entry we want to update and updates it using provided data
+        """
 
         # Uses double-splat to map stored update dict to kwargs
         EateryStore.objects.filter(id=self.id).update(**self.update_data)
--- src/api/views.py
+++ src/api/views.py
@@ -9,6 +9,7 @@ from api.dfg.main import main_dfg
 from api.controllers.create_report import CreateReportController
 from api.controllers.update_eatery import UpdateEateryController
 from api.util.json import verify_json_fields, success_json, error_json, FieldType
+
 # Create your views here.
 
 
@@ -17,12 +18,12 @@ class MainDfgView(APIView):
 
     def get(self, request):
         tzinfo = pytz.timezone("US/Eastern")
-        reload = request.GET.get('reload')
+        reload = request.GET.get("reload")
         result = self.dfg(
             tzinfo=tzinfo,
             reload=reload is not None and reload != "false",
             start=date.today(),
-            end=date.today() + timedelta(days=7)
+            end=date.today() + timedelta(days=7),
         )
         return JsonResponse(result)
 
@@ -30,12 +31,19 @@ class MainDfgView(APIView):
 class ReportView(APIView):
     def post(self, request):
         json_body = json.loads(request.body)
-        if not verify_json_fields(json_body, {"eatery_id": FieldType.INT, "type": FieldType.STRING, "content": FieldType.STRING}):
+        if not verify_json_fields(
+            json_body,
+            {
+                "eatery_id": FieldType.INT,
+                "type": FieldType.STRING,
+                "content": FieldType.STRING,
+            },
+        ):
             return JsonResponse(error_json("Malformed Request"))
         CreateReportController(
             eatery_id=EateryID(json_body["eatery_id"]),
             type=json_body["type"],
-            content=json_body["content"]
+            content=json_body["content"],
         ).process()
         return JsonResponse(success_json("Reported"))
 
@@ -45,13 +53,13 @@ class UpdateView(APIView):
         text_params = request.POST
         try:
             image_param = request.FILES.get("image")
-            print(image_param)
         except:
             image_param = None
-        # Will add JSON verification here
-        UpdateEateryController(
-            text_params.get("id"),
-            text_params,
-            image_param
-        ).process()
-        return JsonResponse(success_json("Updated"))
+
+        try:
+            UpdateEateryController(
+                text_params.get("id"), text_params, image_param
+            ).process()
+            return JsonResponse(success_json("Updated"))
+        except Exception as e:
+            return JsonResponse(error_json(str(e)))
