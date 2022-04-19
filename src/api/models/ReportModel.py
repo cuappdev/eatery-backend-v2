a5c906a69694e5703ac6f8a9823f531dd295e93f
Archit404Error <4architmehta@gmail.com>
2022-04-18 20:49:00 -0400

    Make eatery id optional in reports

--- src/api/models/ReportModel.py
+++ src/api/models/ReportModel.py
@@ -1,8 +1,9 @@
-from django.db import models
 from api.models.EateryModel import EateryStore
+from django.db import models
+
 
 class ReportStore(models.Model):
-    eatery = models.ForeignKey(EateryStore, on_delete=models.DO_NOTHING)
+    eatery = models.ForeignKey(EateryStore, on_delete=models.DO_NOTHING, null=True)
     type = models.CharField(max_length=200)
     content = models.TextField()
     created_timestamp = models.IntegerField()
