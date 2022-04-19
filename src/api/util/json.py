4957e4acf96509cf3503c2354e0bcb4acb0db9e8
Archit404Error <4architmehta@gmail.com>
2022-04-18 21:02:30 -0400

    Edit defensive checks

--- src/api/util/json.py
+++ src/api/util/json.py
@@ -1,18 +1,21 @@
-from typing import Mapping
 from enum import Enum
+from typing import List, Mapping, Optional
 
 from api.datatype.Eatery import EateryID
 
-class FieldType(Enum):
-    INT = 'int'
-    STRING = 'str'
-    EATERYID = "eateryid"
-    
 
-def verify_json_fields(json, field_type_map: Mapping[str, FieldType]):
+class FieldType(Enum):
+    INT = "int"
+    STRING = "str"
+    EATERYID = "eateryid"
+
+
+def verify_json_fields(
+    json, field_type_map: Mapping[str, FieldType], nullable: Optional[List] = []
+):
     for field in field_type_map:
         if field not in json:
-            return False
+            return field in nullable
         if field_type_map[field] is FieldType.INT:
             if not isinstance(json[field], int):
                 return False
@@ -22,19 +25,13 @@ def verify_json_fields(json, field_type_map: Mapping[str, FieldType]):
         elif field_type_map[field] is FieldType.EATERYID:
             if not isinstance(json[field], int) or EateryID(json[field]) == None:
                 return False
-            
+
     return True
 
+
 def success_json(data):
-    return {
-        "success": True,
-        "data": data,
-        "error": None
-    }
+    return {"success": True, "data": data, "error": None}
+
 
 def error_json(error: str):
-    return {
-        "success": False,
-        "data": None,
-        "error": error
-    }
+    return {"success": False, "data": None, "error": error}
