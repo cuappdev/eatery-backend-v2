5c59137c37065b29b82861121c745156d581c51e
Mateo Weiner <mateow99@gmail.com>
2023-03-08 18:19:39 -0500

    Implement user model

--- src/eatery_blue_backend/urls.py
+++ src/eatery_blue_backend/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("event/", include("event.urls")),
     path("cdn/", include("cdn_parser.urls")),
     path("report/", include("report.urls")),
-    path("alert/", include("alert.urls"))
+    path("alert/", include("alert.urls")),
+    path("user/", include("user.urls"))
     #path("wait_time/", include("wait_time.urls"))
 ]
