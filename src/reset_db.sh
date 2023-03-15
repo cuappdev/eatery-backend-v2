d2e1b0145665b9549b8ab2db0aaaeddaf374c29f
Kidus Zegeye <kmz25@cornell.edu>
2023-03-15 18:35:00 -0400

    Modify file structure for docker container compatibility

---
+++ src/reset_db.sh
@@ -0,0 +1,4 @@
+source ../venv/bin/activate
+source ../.envrc
+python3 manage.py makemigrations
+python3 manage.py migrate
--- start.sh
+++
@@ -1,3 +0,0 @@
-source venv/bin/activate
-source .envrc
-python3 src/manage.py runserver 0.0.0.0:8000
