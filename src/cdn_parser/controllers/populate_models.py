2d8453c6d9a389a7bccce724c28d5c74b33b68d9
Kidus Zegeye <kmz25@cornell.edu>
2023-04-05 15:55:24 -0500

    remove menu model

--- src/cdn_parser/controllers/populate_models.py
+++ src/cdn_parser/controllers/populate_models.py
@@ -1,9 +1,10 @@
 import requests 
 from eatery.util.constants import CORNELL_DINING_URL, dining_id_to_internal_id
+from django.core import management
 
 from eatery.controllers.populate_eatery import PopulateEateryController
 from event.controllers.populate_event import PopulateEventController
-from menu.controllers.populate_menu import PopulateMenuController
+#from menu.controllers.populate_menu import PopulateMenuController
 from item.controllers.populate_item import PopulateItemController
 from category.controllers.populate_category import PopulateCategoryController
 """
@@ -49,19 +50,25 @@ class CornellDiningNowController():
         """
 
         json_eateries = self.get_json()
+
         print("Populating eateries")
+        #management.call_command('migrate', 'eatery', 'zero')
+        #management.call_command('migrate', 'eatery')
         PopulateEateryController().process(json_eateries)
 
         print("Populating events")
+        #management.call_command('migrate', 'event', 'zero')
+        #management.call_command('migrate', 'event')
         events_dict = PopulateEventController().process(json_eateries)    
 
-        print("Populating menus")
-        menus_dict = PopulateMenuController().process(events_dict, json_eateries)
-
         print("Populating categories")
-        categories_dict = PopulateCategoryController().process(menus_dict, json_eateries)
+        #management.call_command('migrate', 'category', 'zero')
+        #management.call_command('migrate', 'category')
+        categories_dict = PopulateCategoryController().process(events_dict, json_eateries)
 
         print("Populating items")
+        #management.call_command('migrate', 'item', 'zero')
+        #management.call_command('migrate', 'item')
         PopulateItemController().process(categories_dict, json_eateries)
         print("Done populating")
 
