f20bbca8094b222b400b35ffce5241afdbdbee47
Mateo <maw346@cornell.edu>
2023-09-20 18:30:45 -0400

    remove unused user code

--- src/person/views.py
+++ src/person/views.py
@@ -1,9 +1,6 @@
-import json
 from rest_framework import viewsets
-from rest_framework import generics
 from person.models import Student, Chef
-from person.serializers import StudentSerializer, ChefSerializer, AuthenticateSerializer
-from .controllers.authenticate_controller import AuthenticateController
+from person.serializers import StudentSerializer, ChefSerializer
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -12,14 +9,3 @@ class StudentViewSet(viewsets.ModelViewSet):
 class ChefViewSet(viewsets.ModelViewSet):
     queryset = Chef.objects.all()
     serializer_class = ChefSerializer
-
-class AuthenticateView(generics.GenericAPIView):
-    serializer_class = AuthenticateSerializer
-
-    def post(self, request):
-        """Authenticate the current user."""
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError:
-            data = request.data
-        return AuthenticateController(request, data, self.serializer_class).process()
