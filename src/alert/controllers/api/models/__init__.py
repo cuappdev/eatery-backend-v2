385fa468726db5cc159fcfa2fd09fe0af569d5dc
Mateo <maw346@cornell.edu>
2023-09-20 18:27:31 -0400

    remove old code

--- src/alert/controllers/api/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Need to expose models to django.
-# In this app, models should be imported directly from api.models, not from api.models.package
-
-from eatery.models import EateryStore
-
-#from .alert.models.AlertModel import AlertStore
-#from .EventScheduleModel import EventSchedule, RepeatingEventSchedule, ScheduleException
-#from ...reports.ReportModel import ReportStore
-from .TransactionModel import TransactionHistoryStore
