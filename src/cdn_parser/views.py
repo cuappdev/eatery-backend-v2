9a350bcb53f3b932efc85ae2d3a94ae837b3d474
Mateo <maw346@cornell.edu>
2023-10-17 20:17:10 -0400

    remove unused routes and views

--- src/cdn_parser/views.py
+++ src/cdn_parser/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework import generics
 
-from eatery.util.json import FieldType, error_json, success_json, verify_json_fields
-from cdn_parser.controllers.populate_models import CornellDiningNowController
-
-class PopulateModels(generics.GenericAPIView):
-    def get(self, request):
-        CornellDiningNowController().process()
-        return JsonResponse(success_json("Populated all models"))
-
+# Create your views here.
