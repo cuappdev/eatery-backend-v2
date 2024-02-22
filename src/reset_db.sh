53bbf5ed223d52022b16bd6f96fc1bec5e1141b9
Aayush Agnihotri <aa2328@cornell.edu>
2024-02-22 13:53:41 -0500

    Simplifying reset and start files

--- src/reset_db.sh
+++
@@ -1,5 +0,0 @@
-source ../venv/bin/activate
-source ../.envrc
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py populate_models
--- src/start.sh
+++
@@ -1,3 +0,0 @@
-source ../venv/bin/activate
-source ../.envrc
-python3 manage.py runserver 0.0.0.0:8000
