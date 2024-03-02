e16e5ff3f383a073add7245b2296539e6c6982c5
Daniel Weiner <weinerdanielj@gmail.com>
2024-03-02 16:29:01 -0500

    Events refactor

--- src/event/views.py
+++ src/event/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from event.models import Event
+from event.serializers import EventSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
