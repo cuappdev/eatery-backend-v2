0389cdc2b3a88f5ae206ecdde985812f90949f6c
Aayush Agnihotri <aa2328@cornell.edu>
2024-03-03 14:58:06 -0500

    miscellaneous

--- src/eatery_blue_backend/urls.py
+++ src/eatery_blue_backend/urls.py
@@ -4,6 +4,9 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("eatery/", include("eatery.urls")),
+    path("event/", include("event.urls")),
+    # path("item/", include("item.urls")),
+    path("category/", include("category.urls")),
     path("report/", include("report.urls")),
     path("person/", include("person.urls")),
 ]
