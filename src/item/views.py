655d4a62e8f0bbfbb45afa12b69272d8ffcdd0f7
Daniel Weiner <weinerdanielj@gmail.com>
2024-03-03 12:44:07 -0500

    Category and item crud endpoints

--- src/item/views.py
+++ src/item/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from item.models import Item
+from item.serializers import ItemSerializer
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
