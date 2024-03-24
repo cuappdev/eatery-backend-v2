b544a7787124760d48df9a7d8352a3e179f21e27
Aayush Agnihotri <aa2328@cornell.edu>
2024-03-24 13:19:03 -0400

    Creating users model

--- src/item/models.py
+++ src/item/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from eatery.models import Eatery
 from category.models import Category
 
+
 class Item(models.Model):
-    category = models.ForeignKey(Category, related_name = "items", on_delete=models.DO_NOTHING)
-    name = models.CharField(max_length=40, default = "Item")
+    category = models.ForeignKey(
+        Category, related_name="items", on_delete=models.DO_NOTHING
+    )
+    name = models.CharField(max_length=40, default="Item")
     base_price = models.FloatField(null=True, blank=True, default=0.0)
-    
+
     def __str__(self):
         return f"{self.name} ({self.category.name})"
--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    netid = models.CharField(max_length=40, default="User")
+    token = models.CharField(max_length=40, default="User")
+    name = models.CharField(max_length=40, default="User")
+    favorite_items = models.ManyToManyField(
+        "item.Item", related_name="favorited_by", blank=True
+    )
+    favorite_eateries = models.ManyToManyField(
+        "eatery.Eatery", related_name="favorited_by", blank=True
+    )
+    is_admin = models.BooleanField(default=False)
+    last_active = models.DateTimeField(auto_now=True)
