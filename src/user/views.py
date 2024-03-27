efb96950a7a0f8068680efd59ea6f8f28f0135f1
Daniel Weiner <weinerdanielj@gmail.com>
2024-03-26 20:31:26 -0400

    Add users model

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from user.models import User
+from user.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet:
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
