b81ed2532cb5dd1e004c8ab4ebbf8fa42750b7cd
Aayush Agnihotri <aa2328@cornell.edu>
2024-09-23 18:30:01 -0400

    Adding swagger and bandit

--- src/eatery_blue_backend/settings.py
+++ src/eatery_blue_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework.authtoken",
+    "drf_yasg",
     # Apps
     "eatery_blue_backend",
     "eatery",
