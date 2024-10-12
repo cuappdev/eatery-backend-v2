500c187bb135be70a2c2daf0eea23fbb29974ebe
skyeslattery <dss354@cornell.edu>
2024-10-11 22:11:29 -0400

    create/fix user model

--- src/user/models.py
+++ src/user/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    userId = models.AutoField(primary_key=True)
-    token = models.CharField(max_length=40)
-    refresh_token = models.CharField(max_length=40)
-    netid = models.CharField(max_length=40, default="User")
+    netid = models.CharField(max_length=10, null=True, blank=True)
     name = models.CharField(max_length=40, default="User")
     favorite_items = models.ManyToManyField(
         "item.Item", related_name="favorited_by", blank=True
@@ -14,4 +11,7 @@ class User(models.Model):
         "eatery.Eatery", related_name="favorited_by", blank=True
     )
     is_admin = models.BooleanField(default=False)
-    last_active = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.netid}'
+        
