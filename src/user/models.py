033f9c3f9aa07d70c857ef87b371c5769968b2f0
Cassidy Xu <chx4@cornell.edu>
2024-11-10 13:13:23 -0500

    Change favorite_items field to store item names as strings instead of IDs

--- src/user/models.py
+++ src/user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(models.Model):
@@ -7,13 +8,12 @@ class User(models.Model):
     family_name = models.CharField(max_length=30, null=True, blank=True)
     google_id = models.CharField(max_length=50, unique=True, null=True, blank=True)
     email = models.EmailField(max_length=255, null=True, blank=True)
-    favorite_items = models.ManyToManyField(
-        "item.Item", related_name="favorited_by", blank=True
+    favorite_items = ArrayField(
+        models.CharField(max_length=100), blank=True, default=list
     )
     favorite_eateries = models.ManyToManyField(
         "eatery.Eatery", related_name="favorited_by", blank=True
     )
-    
+
     def __str__(self):
-        return f'{self.netid}'
-        
+        return f"{self.netid}"
